68026d53da2b71d2d70a7ab7c07cc21397d290b1
yyp <yangyipeng@joyame.com>
2024-05-28 16:44:16 +0800

    feat: 部署阿里云-7

--- meeting_room_booking_system_backend/src/app.module.ts
+++ meeting_room_booking_system_backend/src/app.module.ts
@@ -43,26 +43,26 @@ import { StatisticModule } from './statistic/statistic.module';
         TypeOrmModule.forRootAsync({
             useFactory(configService: ConfigService) {
                 return {
-                  type: "mysql",
-                  host: configService.get('mysql_server_host'),
-                  port: configService.get('mysql_server_port'),
-                  username: configService.get('mysql_server_username'),
-                  password: configService.get('mysql_server_password'),
-                  database: configService.get('mysql_server_database'),
-                  synchronize: true,
-                  logging: true,
-                  entities: [
+                    type: "mysql",
+                    host: configService.get('mysql_server_host'),
+                    port: configService.get('mysql_server_port'),
+                    username: configService.get('mysql_server_username'),
+                    password: configService.get('mysql_server_password'),
+                    database: configService.get('mysql_server_database'),
+                    synchronize: true,
+                    logging: true,
+                    entities: [
                         User,
                         Role,
                         Permission,
                         MeetingRoom,
                         Booking
-                  ],
-                  poolSize: 10,
-                  connectorPackage: 'mysql2',
-                  extra: {
-                      authPlugin: 'sha256_password',
-                  }
+                    ],
+                    poolSize: 10,
+                    connectorPackage: 'mysql2',
+                    extra: {
+                        authPlugin: 'sha256_password',
+                    }
                 }
               },
               inject: [ConfigService]
