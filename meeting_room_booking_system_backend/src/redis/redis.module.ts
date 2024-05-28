9c65c2de8df8e4a8c281bece21b01f209fb9af95
yyp <yangyipeng@joyame.com>
2024-05-28 14:08:31 +0800

    feat: 部署阿里云-6

--- meeting_room_booking_system_backend/src/redis/redis.module.ts
+++ meeting_room_booking_system_backend/src/redis/redis.module.ts
@@ -14,11 +14,12 @@ import { RedisService } from './redis.service';
                 const client = createClient({
                     socket: {
                         host: configService.get('redis_server_host'),
-                        port: configService.get('redis_server_port')
+                        port: configService.get('redis_server_port'),
+                        connectTimeout: 30000,
                     },
 
                     // redis 的 database 就是一个命名空间的概念
-                    database: configService.get('redis_server_db')
+                    database: configService.get('redis_server_db'),
                 });
                 await client.connect();
 
