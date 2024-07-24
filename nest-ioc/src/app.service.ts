af536016e30e51f46fd006325e0c3efae0bb99d2
yyp92 <>
2024-07-24 18:34:26 +0800

    feat: IOC解决什么痛点

---
+++ nest-ioc/src/app.service.ts
@@ -0,0 +1,12 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { OtherService } from './other/other.service';
+
+@Injectable()
+export class AppService {
+    @Inject(OtherService) 
+    private otherService:OtherService;
+
+    getHello(): string {
+        return 'Hello World!' + this.otherService.xxx();
+    }
+}
