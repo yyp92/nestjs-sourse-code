c6d51a5699d9b51dd335d74e001be981d77bb773
yyp92 <>
2024-08-05 13:45:12 +0800

    feat: 聊天室-修改密码、修改信息

--- chat-room-backend/src/user/user.controller.ts
+++ chat-room-backend/src/user/user.controller.ts
@@ -1,15 +1,18 @@
-import { Controller, Post, Body, Inject, Get, Query } from '@nestjs/common';
+import { Controller, Post, Body, Inject, Get, Query, BadRequestException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from './user.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { EmailService } from 'src/email/email.service';
 import { RedisService } from 'src/redis/redis.service';
 import { LoginUserDto } from './dto/login-user.dto';
+import { RequireLogin, UserInfo } from 'src/custom.decorator';
+import { UpdateUserPasswordDto } from './dto/update-user-password.dto';
+import { UpdateUserDto } from './dto/udpate-user.dto';
 
 
 @Controller('user')
 export class UserController {
-    constructor(private readonly userService: UserService) {}
+    constructor(private readonly userService: UserService) { }
 
     @Inject(EmailService)
     private emailService: EmailService;
@@ -20,11 +23,13 @@ export class UserController {
     @Inject(JwtService)
     private jwtService: JwtService;
 
+    // 注册
     @Post('register')
     async register(@Body() registerUser: RegisterUserDto) {
         return await this.userService.register(registerUser);
     }
 
+    // 注册 - 发送验证码
     @Get('register-captcha')
     async captcha(@Query('address') address: string) {
         const code = Math.random().toString().slice(2, 8);
@@ -36,10 +41,11 @@ export class UserController {
             subject: '注册验证码',
             html: `<p>你的注册验证码是 ${code}</p>`
         });
-        
+
         return '发送成功';
     }
 
+    // 登录
     @Post('login')
     async userLogin(@Body() loginUser: LoginUserDto) {
         const user = await this.userService.login(loginUser);
@@ -56,5 +62,66 @@ export class UserController {
                 }
             )
         }
+    }
+
+    // 获取个人信息
+    @Get('info')
+    @RequireLogin()
+    async info(@UserInfo('userId') userId: number) {
+        return this.userService.findUserDetailById(userId)
+    }
+
+    // 修改密码
+    @Post('update_password')
+    async updatePassword(@Body() passwordDto: UpdateUserPasswordDto) {
+        return this.userService.updatePassword(passwordDto);
+    }
+
+    // 修改密码 - 发送验证码
+    @Get('update_password/captcha')
+    async updatePasswordCaptcha(@Query('address') address: string) {
+        if (!address) {
+            throw new BadRequestException('邮箱地址不能为空')
+        }
+
+        const code = Math.random().toString().slice(2, 8)
+
+        await this.redisService.set(`update_password_captcha_${address}`, code, 10 * 60)
+
+        await this.emailService.sendMail({
+            to: address,
+            subject: '更改密码验证码',
+            html: `<p>你的更改密码验证码是 ${code}</p>`
+        })
+
+        return '发送成功'
+    }
+
+    // 修改个人信息
+    @Post('update')
+    @RequireLogin()
+    async update(@UserInfo('userId') userId: number, @Body() updateUserDto: UpdateUserDto) {
+        return await this.userService.update(userId, updateUserDto)
+    }
+
+
+    // 修改个人信息 - 发送证码码
+    // 这个接口是登录之后才能访问的，我们从 request.user 中取出 userId，查询下 email 来发送邮件。
+    @Get('update/captcha')
+    @RequireLogin()
+    async updateCaptcha(@UserInfo('userId') userId: number) {
+        const { email: address } = await this.userService.findUserDetailById(userId)
+
+        const code = Math.random().toString().slice(2, 8)
+
+        await this.redisService.set(`update_user_captcha_${address}`, code, 10 * 60)
+
+        await this.emailService.sendMail({
+            to: address,
+            subject: '更改用户信息验证码',
+            html: `<p>你的验证码是 ${code}</p>`
+        })
+
+        return '发送成功'
     }
 }
--- chat-room-backend/src/user/user.service.ts
+++ chat-room-backend/src/user/user.service.ts
@@ -3,6 +3,8 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { RedisService } from 'src/redis/redis.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
+import { UpdateUserPasswordDto } from './dto/update-user-password.dto';
+import { UpdateUserDto } from './dto/udpate-user.dto';
 
 
 @Injectable()
@@ -67,17 +69,116 @@ export class UserService {
                 username: loginUserDto.username
             }
         });
-      
+
         if (!foundUser) {
             throw new HttpException('用户不存在', HttpStatus.BAD_REQUEST);
         }
-      
+
         if (foundUser.password !== loginUserDto.password) {
             throw new HttpException('密码错误', HttpStatus.BAD_REQUEST);
         }
-      
+
         delete foundUser.password;
-        
+
         return foundUser;
-      }
+    }
+
+    async findUserDetailById(userId: number) {
+        const user = await this.prismaService.user.findUnique({
+            where: {
+                id: userId
+            },
+            select: {
+                id: true,
+                username: true,
+                nickName: true,
+                email: true,
+                headPic: true,
+                createTime: true
+            }
+        })
+
+        return user;
+    }
+
+    // 先查询 redis 中相对应的验证码，检查通过之后根据 email 查询用户信息，修改密码之后 save。
+    async updatePassword(passwordDto: UpdateUserPasswordDto) {
+        const captcha = await this.redisService.get(`update_password_captcha_${passwordDto.email}`);
+
+        if (!captcha) {
+            throw new HttpException('验证码已失效', HttpStatus.BAD_REQUEST);
+        }
+
+        if (passwordDto.captcha !== captcha) {
+            throw new HttpException('验证码不正确', HttpStatus.BAD_REQUEST);
+        }
+
+        const foundUser = await this.prismaService.user.findUnique({
+            where: {
+                username: passwordDto.username
+            }
+        });
+
+        foundUser.password = passwordDto.password;
+
+        try {
+            await this.prismaService.user.update({
+                where: {
+                    id: foundUser.id
+                },
+                data: foundUser
+            })
+
+            return '密码修改成功'
+        }
+        catch (e) {
+            this.logger.error(e, UserService)
+
+            return '密码修改失败'
+        }
+    }
+
+    // 根据 userId 查询用户，修改信息后 update 到数据库
+    async update(userId: number, updateUserDto: UpdateUserDto) {
+        const captcha = await this.redisService.get(`update_user_captcha_${updateUserDto.email}`)
+
+        if (!captcha) {
+            throw new HttpException('验证码已失效', HttpStatus.BAD_REQUEST)
+        }
+
+        if (updateUserDto.captcha !== captcha) {
+            throw new HttpException('验证码不正确', HttpStatus.BAD_REQUEST)
+        }
+
+        const foundUser = await this.prismaService.user.findUnique({
+            where: {
+                id: userId
+            }
+        })
+
+        if (updateUserDto.nickName) {
+            foundUser.nickName = updateUserDto.nickName;
+        }
+
+        if (updateUserDto.headPic) {
+            foundUser.headPic = updateUserDto.headPic;
+        }
+
+        try {
+            await this.prismaService.user.update({
+                where: {
+                    id: userId
+                },
+                data: foundUser
+            })
+
+            return '用户信息修改成功'
+        }
+        catch (e) {
+            this.logger.error(e, UserService)
+
+            return '用户信息修改成功'
+        }
+    }
+
 }
